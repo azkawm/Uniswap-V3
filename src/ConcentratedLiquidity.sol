6025347c763e8f95c4efcef16e25ac21237a96d6
azkawm <willianazka427@gmail.com>
2025-03-10 00:06:41 +0700

    added once again

--- src/ConcentratedLiquidity.sol
+++ src/ConcentratedLiquidity.sol
@@ -5,6 +5,7 @@ import {INonfungiblePositionManager} from "@uniswapv3-periphery/interfaces/INonf
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IMockKari} from "./interfaces/IMockKari.sol";
 import {console} from "forge-std/Test.sol";
+import {IUniswapV3Pool} from "@uniswapv3-core/interfaces/IUniswapV3Pool.sol";
 // import '@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol';
 // import '@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol';
 interface IUniswapV3Factory {
@@ -19,6 +20,7 @@ interface IUniswapV3Factory {
 contract ConcentratedLiquidity{
 
     INonfungiblePositionManager public pool;
+    IUniswapV3Pool public poolState;
     IMockKari public kari;
     //IUniswapV3Factory public constant pool = IUniswapV3Factory(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
 
@@ -33,6 +35,7 @@ contract ConcentratedLiquidity{
     uint256 public floorTokenId;
 
     uint256 public discoveryTokenId;
+    uint256 public discovery_LENGTH = 30;
     
     constructor(address _pool, address _baseToken, address _token0, uint24 _fee) {
         pool = INonfungiblePositionManager(_pool); //0xC36442b4a4522E871399CD717aBDD847Ab11FE88
@@ -63,15 +66,13 @@ contract ConcentratedLiquidity{
     //     );
 
     function initPool(uint160 sqrtPriceX96) external {
-        console.log("xxx");
         poolAddress = pool.createAndInitializePoolIfNecessary(baseToken, token0, uniswap_V3_FEE, sqrtPriceX96);
-        // uint256 amount0Desired = 999999948301786405;
-        // uint256 amount1Desired = 3792990;
+        poolState = IUniswapV3Pool(poolAddress);
         uint256 amount0Anchor = 10000e18;
         uint256 amount1Anchor = 1000e6;
         IERC20(token0).transferFrom(msg.sender,address(this), amount1Anchor);
         kari.mint(address(this), amount0Anchor);
-        console.log("KARI", IERC20(baseToken).balanceOf(address(this)));
+        console.log("currentTick", getCurrentTick());
 
         //mint anchor
         INonfungiblePositionManager.MintParams memory anchorParams = INonfungiblePositionManager.MintParams({
@@ -91,12 +92,11 @@ contract ConcentratedLiquidity{
         IERC20(baseToken).approve(address(pool), amount0Anchor);
         IERC20(token0).approve(address(pool), amount1Anchor);
         (uint256 atokenId,,,) = pool.mint(anchorParams);
-
         anchorTokenId = atokenId;
 
+        //mint floor
         uint256 amount1Floor = 1000e6;
         IERC20(token0).transferFrom(msg.sender,address(this), amount1Floor);
-
         INonfungiblePositionManager.MintParams memory floorParams = INonfungiblePositionManager.MintParams({
             token0: baseToken, //KARI
             token1: token0, //AUSD
@@ -113,18 +113,151 @@ contract ConcentratedLiquidity{
 
         IERC20(token0).approve(address(pool), amount1Floor);
         (uint256 ftokenId,,,) = pool.mint(floorParams);
-
         floorTokenId = ftokenId;
+        //mint discovery
+        kari.mint(address(this), 10000e18);
+        uint256 amount0Discovery = IERC20(baseToken).balanceOf(address(this));
+        INonfungiblePositionManager.MintParams memory discoveryParams = INonfungiblePositionManager.MintParams({
+            token0: baseToken, //KARI
+            token1: token0, //AUSD
+            fee: 3000, //menentukan harga
+            tickLower: -276240, //menentukan harga
+            tickUpper: -274440, //menentukan harga
+            amount0Desired: amount0Discovery,
+            amount1Desired: 0,
+            amount0Min: 0,
+            amount1Min: 0,
+            recipient: address(this),
+            deadline: block.timestamp
+        });
 
-        // kari.mint(address(this), 10000e18);
-        // uint256 amount0Discovery = IERC20(baseToken).balanceOf(address(this));
+        IERC20(baseToken).approve(address(pool), amount0Discovery);
+        (uint256 dtokenId,,,) = pool.mint(discoveryParams);
+        discoveryTokenId = dtokenId;
+    }
 
+    function getCurrentTick() public view returns(int24){
+            // uint160 sqrtPriceX96,
+            // int24 tick,
+            // uint16 observationIndex,
+            // uint16 observationCardinality,
+            // uint16 observationCardinalityNext,
+            // uint8 feeProtocol,
+            // bool unlocked
+        (,int24 tick,,,,,) = poolState.slot0();
+        return tick;
+    }
+
+    function move() external{
+            //move anchor to discovery
+            //anchor
+            //collect
+            pool.collect(
+                INonfungiblePositionManager.CollectParams({
+                    tokenId: anchorTokenId,
+                    recipient: address(this),
+                    amount0Max: type(uint128).max,
+                    amount1Max: type(uint128).max
+                })
+            );
+
+            // withdraw semua/decreaseLiquidity
+            (,,,,,,, uint128 aliquidity,,,,) = pool.positions(anchorTokenId);
+
+            pool.decreaseLiquidity(
+                INonfungiblePositionManager.DecreaseLiquidityParams({
+                    tokenId: anchorTokenId,
+                    liquidity: aliquidity,
+                    amount0Min: 0,
+                    amount1Min: 0,
+                    deadline: block.timestamp
+                })
+            );
+
+            // burn
+            pool.collect(
+                INonfungiblePositionManager.CollectParams({
+                    tokenId: anchorTokenId,
+                    recipient: address(this),
+                    amount0Max: type(uint128).max,
+                    amount1Max: type(uint128).max
+                })
+            );
+            pool.burn(anchorTokenId);
+
+            //discovery
+            //collect
+            pool.collect(
+                INonfungiblePositionManager.CollectParams({
+                    tokenId: discoveryTokenId,
+                    recipient: address(this),
+                    amount0Max: type(uint128).max,
+                    amount1Max: type(uint128).max
+                })
+            );
+
+            // withdraw semua/decreaseLiquidity
+            (,,,,,,, uint128 dliquidity,,,,) = pool.positions(discoveryTokenId);
+            
+            pool.decreaseLiquidity(
+                INonfungiblePositionManager.DecreaseLiquidityParams({
+                    tokenId: discoveryTokenId,
+                    liquidity: dliquidity,
+                    amount0Min: 0,
+                    amount1Min: 0,
+                    deadline: block.timestamp
+                })
+            );
+
+            pool.collect(
+                INonfungiblePositionManager.CollectParams({
+                    tokenId: discoveryTokenId,
+                    recipient: address(this),
+                    amount0Max: type(uint128).max,
+                    amount1Max: type(uint128).max
+                })
+            );
+            
+            // burn
+            pool.burn(discoveryTokenId);
+            
+            uint256 amount0Anchor = IERC20(baseToken).balanceOf(address(this));
+            uint256 amount1Anchor = IERC20(token0).balanceOf(address(this));
+            
+            uint256 amount0Discovery = IERC20(baseToken).balanceOf(address(this)) - amount0Anchor;
+        //mint anchor
+        INonfungiblePositionManager.MintParams memory anchorParams = INonfungiblePositionManager.MintParams({
+            token0: baseToken, //KARI
+            token1: token0, //AUSD
+            fee: 3000, //menentukan harga
+            tickLower: -277320, //menentukan harga 
+            tickUpper: -274920, //menentukan harga
+            amount0Desired: amount0Anchor,
+            amount1Desired: amount1Anchor,
+            amount0Min: 0,
+            amount1Min: 0,
+            recipient: address(this),
+            deadline: block.timestamp
+        });
+        console.log("tickLower", -277320 );
+        console.log("tickLower", -274920);
+        console.log("kari balance", IERC20(baseToken).balanceOf(address(this)));
+        console.log("ausd balance", IERC20(token0).balanceOf(address(this)));
+
+        IERC20(baseToken).approve(address(pool), amount0Anchor);
+        IERC20(token0).approve(address(pool), amount1Anchor);
+        (uint256 atokenId,,,) = pool.mint(anchorParams);
+        anchorTokenId = atokenId;
+        console.log("kari balance", IERC20(baseToken).balanceOf(address(this)));
+        console.log("ausd balance", IERC20(token0).balanceOf(address(this)));
+
+        // //mint discovery
         // INonfungiblePositionManager.MintParams memory discoveryParams = INonfungiblePositionManager.MintParams({
         //     token0: baseToken, //KARI
         //     token1: token0, //AUSD
         //     fee: 3000, //menentukan harga
-        //     tickLower: -276420, //menentukan harga 
-        //     tickUpper: -276300, //menentukan harga
+        //     tickLower: -274440, //menentukan harga
+        //     tickUpper: -272640, //menentukan harga
         //     amount0Desired: amount0Discovery,
         //     amount1Desired: 0,
         //     amount0Min: 0,
@@ -133,9 +266,8 @@ contract ConcentratedLiquidity{
         //     deadline: block.timestamp
         // });
 
-        // IERC20(token0).approve(address(pool), amount0Discovery);
+        // IERC20(baseToken).approve(address(pool), amount0Discovery);
         // (uint256 dtokenId,,,) = pool.mint(discoveryParams);
-
         // discoveryTokenId = dtokenId;
     }
 }
--- test/ConcentratedLiquidity.t.sol
+++ test/ConcentratedLiquidity.t.sol
@@ -6,11 +6,16 @@ import {ConcentratedLiquidity} from "../src/ConcentratedLiquidity.sol";
 import {MockAUSD} from "../src/mocks/tokens/MockAUSD.sol";
 import {MockKARI} from "../src/mocks/tokens/MockKARI.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ISwapRouter} from "@uniswapv3-periphery/interfaces/ISwapRouter.sol";
 
 contract ConcentratedLiquidityTest is Test {
     ConcentratedLiquidity public concentratedLiquidity;
+    ISwapRouter public swapRouter;
     MockAUSD public ausd;
     MockKARI public kari;
+
+    address router = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    address alice = makeAddr("alice");
     // address public ausd = address(mockAusd);
     // address public kari = address(mockKari);
 
@@ -20,20 +25,48 @@ contract ConcentratedLiquidityTest is Test {
 
         ausd = new MockAUSD();
         kari = new MockKARI();
+        swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
 
         //address _pool, address _baseToken, address _token0, uint24 _fee
         concentratedLiquidity = new ConcentratedLiquidity(0xC36442b4a4522E871399CD717aBDD847Ab11FE88, address(kari), address(ausd), 3000);
         deal(address(ausd), address(this), 10000e6);
         deal(address(kari), address(this), 10000e18);
+        deal(address(ausd), alice, 2000e6);
+
+        IERC20(ausd).approve(address(concentratedLiquidity), 2000e6);
+        concentratedLiquidity.initPool(79224306130848112672356);
     }
 
     function test_initPool() public {
         IERC20(ausd).approve(address(concentratedLiquidity), 2000e6);
-        console.log("AUSD", IERC20(ausd).balanceOf(address(this)));
-        //kari.mint(address(concentratedLiquidity), 999999948301786405);
-        //kari.mint(address(concentratedLiquidity), 10000e18);
-        //console.log("KARI", IERC20(kari).balanceOf(address(concentratedLiquidity)));
         concentratedLiquidity.initPool(79224306130848112672356);
-        console.log("NFT Id", concentratedLiquidity.anchorTokenId());
+        console.log("Anchor Id", concentratedLiquidity.anchorTokenId());
+        console.log("Floor Id", concentratedLiquidity.floorTokenId());
+        console.log("Discovery Id", concentratedLiquidity.discoveryTokenId());
     }
+
+    function test_move() external{
+        console.log("current Tick", concentratedLiquidity.getCurrentTick());
+        uint256 swapAmount = 1000e6;
+        //swap 10 ausd for kari
+        ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: address(ausd),
+                tokenOut: address(kari),
+                fee: 3000, // 0.3
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountIn: swapAmount,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+        vm.startPrank(alice);
+        IERC20(ausd).approve(router, swapAmount); // approve kepada Uniswap
+        swapRouter.exactInputSingle(params);
+        vm.stopPrank();
+        console.log("post Current Tick", concentratedLiquidity.getCurrentTick());
+
+        concentratedLiquidity.move();
+    }
+
 }
